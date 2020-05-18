2a17bd05b4381e90807acbc830019bbc54bd7d1e
mmasso <mmasso@cifpfbmoll.eu>
2020-05-18 18:30:10 +0200

    feat(P12_03): falta comprovació

--- P12_03.js
+++ P12_03.js
@@ -1,4 +1,5 @@
-/*1.- Create a database and a collection inside to keep data about the high schools in the Balearic island. The documents must have the following fields:
+/*1.- Create a database and a collection inside to keep data about the high schools in the Balearic island.
+The documents must have the following fields:
 Name.
 E-mail.
 Creation date.
@@ -61,9 +62,57 @@ db.createCollection('HighSchools', {
 //Validate the schema of the collection.
 //2.- Using insertOne, insert a single document.
 
+db.post.insertOne({
+    _id: "SCH01",
+    Name: "IES SINEU",
+    Email: "ies.sineu@gmail.com",
+    CreationDate: ISODate("1999-01-01"),
+    TelephoneNumber: "921542365",
+    Address: "Carrer dels aires, 23",
+    Studies: {
+        Name: "Informática",
+        Description: "Para gente con gafas"
+    }
+});
 
 
+//3.- Using insertMany, insert some documents but deliberately insert the same "_id" that the 
+//one in exercise 2 to get an error. Use unordered inserts not to stop the insertions despite that error.
 
-
-
-//3.- Using insertMany, insert some documents but deliberately insert the same "_id" that the one in exercise 2 to get an error. Use unordered inserts not to stop the insertions despite that error.
+db.post.insertMany([{
+        _id: "SCH02",
+        Name: "IES SON MARÇAL",
+        Email: "ies.son.marçal@gmail.com",
+        CreationDate: ISODate("1982-01-01"),
+        TelephoneNumber: "921542895",
+        Address: "Carrer dels vents, 24",
+        Studies: {
+            Name: "Biologia",
+            Description: "Para gente con pelo"
+        }
+    },
+    {
+        _id: "SCH01",
+        Name: "IES SON PEDRO",
+        Email: "ies.son.pedro@gmail.com",
+        CreationDate: ISODate("1992-01-01"),
+        TelephoneNumber: "991542895",
+        Address: "Carrer de les bufades, 54",
+        Studies: {
+            Name: "Enginyeria",
+            Description: "Para gente con oido"
+        }
+    },
+    {
+        _id: "SCH03",
+        Name: "IES SON JOSEP",
+        Email: "ies.son.josep@gmail.com",
+        CreationDate: ISODate("1998-01-01"),
+        TelephoneNumber: "921112895",
+        Address: "Carrer dels tifons, 23",
+        Studies: {
+            Name: "Nautica",
+            Description: "Para gente con pies"
+        }
+    }
+], { ordered: false });
